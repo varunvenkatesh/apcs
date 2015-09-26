dffa9c8beeabc2bf075bc1410a8ee32700f4bb4b
kushal tirumala <kushaltirumala99@gmail.com>
2015-09-25 21:29:55 -0700

    myMath + parallel

--- A12_6ParallelLines/ParallelLines.java
+++ A12_6ParallelLines/ParallelLines.java
@@ -12,59 +12,61 @@ import javax.swing.*;
  *          Sources - Kushal Tirumala
  */
 public class ParallelLines extends JPanel {
-	
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g); 
-		int width = getWidth();
-		int height = getHeight();
+    /**
+     * paints the JPanel screen
+     * @param g Graphics object that controls the JPanel
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        int width = getWidth();
+        int height = getHeight();
 
-		drawIllusion(g, width, height);
-	}
+        drawIllusion(g, width, height);
+    }
 
-	/**
-	 * Draws the parallel lines
-	 * 
-	 * @param g
-	 * @param width
-	 * @param height
-	 * @param size
-	 */
-	public void drawIllusion(Graphics g, int width, int height) {
-		g.setColor(Color.black);
-		int nextX = 0;
-		int nextY = 0;
-		int offset;
+    /**
+     * Draws the parallel lines
+     * 
+     * @param g Graphics object
+     * @param width width of screen/as big as you want grid to be
+     * @param height height of screen/as tall as you want grid
+     */
+    public void drawIllusion(Graphics g, int width, int height) {
+        g.setColor(Color.black);
+        int nextX = 0;
+        int nextY = 0;
+        int offset;
+        
+        for (int i = 0; i < 8; i++) {
+            nextX = 0;
+            offset = 8 * (i % 4);
+            nextX += offset;
+            g.drawLine(nextX - 80, nextY, width, nextY);
+            for (int j = 0; j < 7; j++) {
+                g.fillRect(nextX, nextY, 40, 40);
+                nextX += 80;
+            }
 
-		for (int i = 0; i < 8; i++) {
-			nextX = 0;
-			offset = 8 * (i % 4);
-			nextX += offset;
-			g.drawLine(nextX - 80, nextY, width, nextY);
-			for (int j = 0; j < 7; j++) {
-				g.fillRect(nextX, nextY, 40, 40);
-				nextX += 80;
-			}
+            nextY += 40;
+        }
+    }
 
-			nextY += 40;
-		}
-	}
-
-	/**
-	 * Mainmethod to test the parallel lines class
-	 * 
-	 * @param args
-	 *            command line arguments
-	 */
-	public static void main(String[] args) {
-		JFrame w = new JFrame("ParallelLines");
-		w.setBounds(100, 100, 640, 480);
-		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ParallelLines panel = new ParallelLines();
-		panel.setBackground(Color.WHITE);
-		Container c = w.getContentPane();
-		c.add(panel);
-		w.setResizable(true);
-		w.setVisible(true);
-	}
+    /**
+     * Mainmethod to test the parallel lines class
+     * 
+     * @param args
+     *            command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame w = new JFrame("ParallelLines");
+        w.setBounds(100, 100, 640, 480);
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ParallelLines panel = new ParallelLines();
+        panel.setBackground(Color.WHITE);
+        Container c = w.getContentPane();
+        c.add(panel);
+        w.setResizable(true);
+        w.setVisible(true);
+    }
 }
---
+++ JMCh08_PerfectNumbers/MyMath.java
@@ -0,0 +1,171 @@
+/**
+ * Finds a various number of mathematically significant numbers
+ *
+ * @author Kushal Tirumala
+ * @version 9/24/2015
+ * @author Period: 6
+ * @author Assignment: JMCh08_PerfectNumbers
+ *
+ * @author Sources: Kushal Tirumala
+ */
+public class MyMath {
+    /**
+     * Returns true if n is a prime; otherwise returns false.
+     * 
+     * @param n
+     *            number to check if prime
+     * @return true if n is a prime; otherwise returns false
+     */
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        int m = 2;
+
+        while (m * m <= n) {
+            if (n % m == 0) {
+                return false;
+            }
+            m++;
+        }
+
+        return true;
+    }
+
+    /**
+     * Question 8-15 Returns the n-th Fibonacci number
+     * 
+     * Find and read a few informative websites dedicated to Fibonacci numbers.
+     * The following recursive method returns the n-th Fibonacci number:
+     * 
+     * // Returns the n-th Fibonacci number. // Precondition: n >= 1 public
+     * static long fibonacci(int n) { if (n == 1 || n == 2) return 1; else
+     * return fibonacci(n - 1) + fibonacci(n - 2); } Rewrite it without
+     * recursion, using one loop.
+     *
+     * @param n
+     *            Fibonacci number to find
+     * @return n-th Fibonacci number
+     */
+    public static long fibonacci(int n) {
+        
+        int ans = 1;
+        int prev = 1;
+        for (int i = 2; i < n; ++i) {
+            int temp = ans;
+            ans += prev;
+            prev = temp;
+        }
+
+
+        return ans; // ** FIX THIS
+    }
+
+    /**
+     * Returns true if n is a perfect number, false otherwise.
+     * 
+     * @param n
+     *            number to test
+     * @return true if n is a perfect number, false otherwise.
+     */
+    public static boolean isPerfect(int n) {
+        int sum = 0;
+		
+        for (int i = 1; i < n; i++) {
+			
+            if (n % i == 0 ) {
+                sum += i;
+				
+            }
+        }
+		
+        return sum == n;
+
+    }
+
+    /**
+     * Prints the first n perfect numbers
+     * 
+     * @param n
+     *            number of perfect numbers to print
+     */
+    public static void printPerfectNums(int n) {
+        System.out.println("Perfect numbers: ");
+        int count = 0;
+        for (int k = 1; count < n; k++) {
+            if (isPerfect(k)) {
+                System.out.print(k + " ");
+                count++;
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Prints the first n Mersenne primes
+     * 
+     * @param n
+     *            number of Mersenne primes to print
+     */
+    public static void printMersennePrimes(int n) {
+        System.out.println("Mersenne primes: ");
+
+        int currentNum = 0;
+        int i = 0;
+        while (currentNum <= n) {
+            int num = (int)Math.pow(2, i) - 1;
+            if (isPrime(num)) {
+                System.out.println(num);
+                currentNum++;
+            }
+            i++;
+			
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * Prints the first n even perfect numbers
+     * @param n the number of perfect even numbers you want 
+     *
+     */
+    public static void printEvenPerfectNums(int n) {
+        System.out.println("Even perfect numbers: ");
+
+        int currentNum = 0;
+        int i = 0;
+        while (currentNum <= n) {
+            int num = (int)Math.pow(2, i) - 1;
+			
+            if (isPrime(num)) {
+                long perfNumber = (long)Math.pow(2, i - 1);
+                System.out.println(perfNumber * num);
+                currentNum++;
+            }
+            i++;
+			
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * the main method to test out all the functions abov
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        int n = 19;
+        System.out.println(n + "-th Fibonacci number = " + fibonacci(n));
+
+        printPerfectNums(4);
+        printMersennePrimes(6);
+        printEvenPerfectNums(6);
+    }
+}
+/*
+ * 19-th Fibonacci number = 4181 Perfect numbers: 6 28 496 8128 Mersenne primes:
+ * 3 7 31 127 8191 131071 524287 Even perfect numbers: 6 28 496 8128 33550336
+ * 8589869056 137438691328
+ */
